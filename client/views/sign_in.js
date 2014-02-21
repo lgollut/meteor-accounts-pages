240ecde41aa0dfbae17748926fa5cbcf3a16b486
Louis Gollut <louis.gollut@gmail.com>
2014-02-21 16:30:16 +0100

    Added sign in base functionnality

---
+++ client/views/sign_in.js
@@ -0,0 +1,57 @@
+Template.signIn.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    isValid = $('.ui.form').form('validate form');
+
+    if(!isValid) {
+      return false;
+    }
+
+    Session.set('login', $('input[name="login"]').val());
+    Session.set('password', $('input[name="password"]').val());
+
+    Meteor.loginWithPassword(Session.get('login'), Session.get('password'), function(error) {
+      Session.set('password', undefined);
+
+      if(error) {
+        alert(error);
+      } else if(Session.get('fromWhere')) {
+
+      } else {
+        Router.go('/');
+      }
+    })
+  }
+})
+
+Template.signIn.rendered = function() {
+
+  $('.ui.form').form({
+    login: {
+      identifier: 'login',
+      rules: [
+        {
+          type: 'empty',
+          prompt : 'You must enter your login'
+        }
+      ]
+    },
+    password : {
+      identifier: 'password',
+      rules: [
+        {
+          type: 'empty',
+          prompt: 'You must enter your password'
+        },{
+          type: 'length[6]',
+          prompt: 'Your password must be at least 6 characters'
+        }
+      ]
+    }
+  }, {
+    keyboardShortcuts: false,
+    inline: true,
+    on: 'blur'
+  });
+}
