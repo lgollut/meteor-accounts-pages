6df5752e58da8ef6ea63599b567da6e626d81c51
Louis Gollut <louis.gollut@gmail.com>
2014-02-21 16:29:44 +0100

    Added sign up base functionnality

---
+++ client/views/sign_up.js
@@ -0,0 +1,70 @@
+Template.signUp.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    isValid = $('.ui.form').form('validate form');
+
+    if(!isValid) {
+      return false;
+    }
+
+    userInfo = {
+      username: $('input[name="username"]').val(),
+      email: $('input[name="email"]').val(),
+      password: $('input[name="password"]').val(),
+      profile: {}
+    }
+
+    Accounts.createUser(userInfo, function(error) {
+      if(error) {
+        alert(error);
+        return;
+      };
+
+      Router.go('home');
+    });
+  }
+})
+
+Template.signUp.rendered = function() {
+
+  $('.ui.form').form({
+    username: {
+      identifier: 'username',
+      rules: [
+        {
+          type: 'empty',
+          prompt : 'You must enter your user name'
+        }
+      ]
+    },
+    email: {
+      identifier: 'email',
+      rules: [
+        {
+          type: 'empty',
+          prompt : 'You must enter your email address'
+        }, {
+          type: 'email',
+          prompt: 'You must enter a valid email address'
+        }
+      ]
+    },
+    password : {
+      identifier: 'password',
+      rules: [
+        {
+          type: 'empty',
+          prompt: 'You must enter your password'
+        }, {
+          type: 'length[6]',
+          prompt: 'Your password must be at least 6 characters'
+        }
+      ]
+    }
+  }, {
+    keyboardShortcuts: false,
+    inline: true,
+    on: 'blur'
+  });
+}
