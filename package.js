836f14ebddc17d89c1329a751c18529f92fc70af
Louis Gollut <louis.gollut@gmail.com>
2014-02-24 08:45:59 +0100

    Update package file with proper api configuration

--- package.js
+++ package.js
@@ -1,13 +1,34 @@
 Package.describe({
-  summary: "REPLACEME - What does this package (or the original one you're wrapping) do?"
+  summary: "Meteor smart package for full authentication pages styled with Semantic UI"
 });
 
 Package.on_use(function (api, where) {
-  api.add_files('accounts-pages.js', ['client', 'server']);
+
+  // client
+  api.use([
+    'handlebars',
+    'less',
+    'templating'
+    ], 'client');
+
+  api.add_files([
+    'client/views/account_button.html',
+    'client/views/account_button.js',
+    'client/views/sign_in.html',
+    'client/views/sign_in.js',
+    'client/views/forgot_password.html',
+    'client/views/forgot_password.js',
+    'client/views/sign_up.html',
+    'client/views/sign_up.js'
+  ], 'client');
+
+  // client and server
+  api.export('AccountsPages', ['client', 'server']);
+  api.use('iron-router', ['client', 'server']);
+  api.add_files('lib/router.js', ['client', 'server']);
+
 });
 
 Package.on_test(function (api) {
-  api.use('accounts-pages');
 
-  api.add_files('accounts-pages_tests.js', ['client', 'server']);
 });
